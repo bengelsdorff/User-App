02f5c89b681fcc81cd78665b76cf21fa0bef7ea9
Antonio Bengelsdorff <antonio.bengelsdorff@gmail.com>
2016-03-21 16:05:54 +0000

    Add missing file

---
+++ UserApp/UserApp.WebSite.Tests/Controllers/UserControllerTest.cs
@@ -0,0 +1,35 @@
+﻿using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UserApp.Controllers;
+using UserApp.Core.Interfaces;
+using Moq;
+using System;
+using UserApp.Domain;
+using System.Threading.Tasks;
+
+namespace UserApp.Tests.Controllers
+{
+    [TestClass]
+    public class UserControllerTest
+    {
+        [TestMethod]
+        public void SignIn_Is_Not_Null()
+        {
+            // Arrange
+            var mockUserBusinessLogic = Mock.Of<IUserBusinessLogic>();
+            Mock.Get(mockUserBusinessLogic)
+               .Setup(s => s.SignIn(string.Empty, string.Empty))
+               .ReturnsAsync(new Tuple<AuthenticationStatus, User>(AuthenticationStatus.Invalid, null));
+
+            UserController controller = new UserController(mockUserBusinessLogic);
+
+            // Act
+            ViewResult result = controller.SignIn() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+
+    }
+}
